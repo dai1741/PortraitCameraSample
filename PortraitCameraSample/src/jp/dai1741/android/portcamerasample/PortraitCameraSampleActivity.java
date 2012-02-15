67663b0db1c745276c95af71b87a6904e654edd7
dai1741 <fujita.dai@gmail.com>
2012-02-15 18:38:31 +0900

    First commit

---
+++ PortraitCameraSample/src/jp/dai1741/android/portcamerasample/PortraitCameraSampleActivity.java
@@ -0,0 +1,13 @@
+package jp.dai1741.android.portcamerasample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PortraitCameraSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
