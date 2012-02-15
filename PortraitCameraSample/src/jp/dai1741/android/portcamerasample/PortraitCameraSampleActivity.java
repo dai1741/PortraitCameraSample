d2ab3c4f7fba315c714ff1b6d37083bb429e9509
dai1741 <fujita.dai@gmail.com>
2012-02-15 18:57:33 +0900

    Display camera preview in PortraitCameraSampleActivity
    Additionally, added camera permission, changed layout xml.

--- PortraitCameraSample/src/jp/dai1741/android/portcamerasample/PortraitCameraSampleActivity.java
+++ PortraitCameraSample/src/jp/dai1741/android/portcamerasample/PortraitCameraSampleActivity.java
@@ -2,12 +2,19 @@ package jp.dai1741.android.portcamerasample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class PortraitCameraSampleActivity extends Activity {
+    private CameraPreviewView mCameraPreviewView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mCameraPreviewView = new CameraPreviewView(this);
+        ViewGroup root = (ViewGroup)findViewById(R.id.root_layout);
+        root.addView(mCameraPreviewView);
     }
 }
